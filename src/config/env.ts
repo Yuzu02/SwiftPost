443cff478b1ab72d392864b74012d3438070a5b6
Yuzu <Yuzu_0204@outlook.com>
2025-03-21 02:19:46 -0400

    feat: :sparkles: Add dotenv for environment variable management, set up API prefix, and restructure application module

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,22 +1,28 @@
-/* eslint-disable @typescript-eslint/no-namespace */
+import 'dotenv/config'; // Load environment variables from .env file
 import { z } from 'zod';
 
 const envVars = z
   .object({
-    PORT: z.string().default('3000').transform(Number),
+    PORT: z.string().transform(Number).default('3000'), // Default port for the server
+    API_PREFIX: z.string().default('api'), // Default prefix for the API
   })
   .passthrough();
 
+// safeParse to validate the environment variables without stop the app
 const parseEnv = envVars.safeParse(process.env);
 
 if (!parseEnv.success) {
-  console.error('Invalid environment variables:', parseEnv.error.format());
-  throw new Error('Invalid environment variables');
+  console.error('❌ Invalid environment variables:');
+  console.error('⚠️ ', parseEnv.error.format());
+  throw new Error('🚫 Invalid environment variables');
 }
 
-declare global {
-  namespace NodeJS {
-    // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-    interface ProcessEnv extends z.infer<typeof envVars> {}
-  }
-}
+// Variable to use across the entire application
+export const env = {
+  port: parseEnv.data.PORT,
+  apiPrefix: parseEnv.data.API_PREFIX,
+  // Add more environment variables as needed
+};
+
+// Type to have the environment variables in a type-safe way
+export type Env = z.infer<typeof envVars>;
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,37 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { env } from '@/config/env';
+import { Logger, RequestMethod, ValidationPipe } from '@nestjs/common';
+import { SwiftPostModule } from './swiftpost.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(SwiftPostModule);
+  const logger = new Logger('SwiftPost');
+
+  //* Globals
+
+  app.setGlobalPrefix(env.apiPrefix, {
+    exclude: [
+      {
+        path: '/',
+        method: RequestMethod.GET,
+      },
+    ],
+  }); // Set global prefix for all routes
+
+  app.enableCors(); // Enable CORS for all routes and origins
+
+  // Enable Global Pipes for Validation
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  await app.listen(env.port, () => {
+    const purpleUrl = `\x1b[35mhttp://localhost:${env.port}\x1b[0m`;
+    logger.log(`🚀 Server is running on ${purpleUrl}`);
+  });
 }
+
 bootstrap();
